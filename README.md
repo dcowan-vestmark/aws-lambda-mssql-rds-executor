f19ffd32db1addb816183279fe868945c0fe46f9
David Cowan <dcowan@vestmark.com>
2020-07-01 16:03:43 -0500

    Cleanup

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # aws-lambda-mssql-rds-executor
+
+This lambda package will take an environment variable SQL_SCRIPT and run that script upon execution.  It is designed to run a script based on events from an MSSQL RDS instance.  For example on reboot execute a script to add permissions to the tempdb.  The password is passed in via the environment variable RDS_PASSWORD.  The rest of the db parameters are pulled from the RDS db instance api.
+
+
+Suggested AWS Policy to use for VPC based lambda
+```
+"document": {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Effect": "Allow",
+        "Action": [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "rds:DescribeDBInstances",
+            "ec2:CreateNetworkInterface",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:DeleteNetworkInterface"
+        ],
+        "Resource": "*"
+        }
+    ]
+}
+```
--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 const rds = require('aws-sdk/clients/rds'),
-      sns = require('aws-sdk/clients/sns'),
-      fs = require('fs'),
-      sql = require('mssql');
+	sns = require('aws-sdk/clients/sns'),
+	fs = require('fs'),
+	sql = require('mssql');
 
 const awsClient = new rds();
 
 exports.handler = async (event, context) => {
-  console.log("Event=" + JSON.stringify(event))
-  const record = JSON.parse(event.Records[0].Sns.Message);
+	console.log("Event=" + JSON.stringify(event))
+	const record = JSON.parse(event.Records[0].Sns.Message);
 
-  if (record["Event Source"] != "db-instance") { return; }
-  //if (record["Event Message"] != "DB instance created") { return; }
+	if (record["Event Source"] != "db-instance") {
+		return;
+	}
+	//if (record["Event Message"] != "DB instance created") { return; }
 
-  console.log("Requesting instance information for: " + record["Source ID"]);
-  var data = await awsClient.describeDBInstances(
-    {DBInstanceIdentifier: record["Source ID"]}
-  ).promise();
+	console.log("Requesting instance information for: " + record["Source ID"]);
+	var data = await awsClient.describeDBInstances({
+		DBInstanceIdentifier: record["Source ID"]
+	}).promise();
 
-  if (data.DBInstances.length == 0) { 
-    console.log("No DB Instance found")
-    return; 
-  }
+	if (data.DBInstances.length == 0) {
+		console.log("No DB Instance found")
+		return;
+	}
 
-  var instance = data.DBInstances[0];
+	var instance = data.DBInstances[0];
 
-  try {
-      const config = {
-          user: instance.MasterUsername,
-          password: process.env.RDS_PASSWORD,
-          server: instance.Endpoint.Address, 
-          database: 'tempdb',
-          options: {
-            enableArithAbort: true
-          }
-      }
-      console.log("connecting to " + instance.Endpoint.Address)
-      await sql.connect(config)
-      const result = await sql.query(process.env.SQL_SCRIPT)
-      console.dir(result)
-  } catch (err) {
-    console.error("Error executing sql: " + err)
-  }
+	try {
+		const config = {
+			user: instance.MasterUsername,
+			password: process.env.RDS_PASSWORD,
+			server: instance.Endpoint.Address,
+			database: 'tempdb',
+			options: {
+				enableArithAbort: true
+			}
+		}
+		console.log("connecting to " + instance.Endpoint.Address)
+		await sql.connect(config)
+		const result = await sql.query(process.env.SQL_SCRIPT)
+		console.dir(result)
+	} catch (err) {
+		console.error("Error executing sql: " + err)
+	}
 };
