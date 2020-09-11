04dc428ded68bc0be9139999773b989c5e37e4d5
David Cowan <dcowan@vestmark.com>
2020-09-11 10:29:21 -0500

    Adding error handling and formatting

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-const rds = require('aws-sdk/clients/rds'),
-	sns = require('aws-sdk/clients/sns'),
-	fs = require('fs'),
-	sql = require('mssql');
+const rds = require("aws-sdk/clients/rds"),
+  sns = require("aws-sdk/clients/sns"),
+  fs = require("fs"),
+  sql = require("mssql");
 
 const awsClient = new rds();
 
 exports.handler = async (event, context) => {
-	console.log("Event=" + JSON.stringify(event))
-	const record = JSON.parse(event.Records[0].Sns.Message);
+  console.log("Event=" + JSON.stringify(event));
+  const record = JSON.parse(event.Records[0].Sns.Message);
 
-	if (record["Event Source"] != "db-instance") {
-		return;
-	}
-	//if (record["Event Message"] != "DB instance created") { return; }
+  if (record["Event Source"] != "db-instance") {
+	console.log("Exiting due to no db-instance Event Source");
+	context.fail("Exiting due to no db-instance Event Source");
+    return;
+  }
+ 
+  console.log("Requesting instance information for: " + record["Source ID"]);
+  var data = await awsClient
+    .describeDBInstances({
+      DBInstanceIdentifier: record["Source ID"],
+    })
+    .promise();
 
-	console.log("Requesting instance information for: " + record["Source ID"]);
-	var data = await awsClient.describeDBInstances({
-		DBInstanceIdentifier: record["Source ID"]
-	}).promise();
+  if (data.DBInstances.length == 0) {
+	console.log("No DB Instance found");
+	context.fail("No DB Instance found");
+    return;
+  }
 
-	if (data.DBInstances.length == 0) {
-		console.log("No DB Instance found")
-		return;
-	}
+  var instance = data.DBInstances[0];
 
-	var instance = data.DBInstances[0];
-
-	try {
-		const config = {
-			user: instance.MasterUsername,
-			password: process.env.RDS_PASSWORD,
-			server: instance.Endpoint.Address,
-			database: 'tempdb',
-			options: {
-				enableArithAbort: true
-			}
-		}
-		console.log("connecting to " + instance.Endpoint.Address)
-		await sql.connect(config)
-		const result = await sql.query(process.env.SQL_SCRIPT)
-		console.dir(result)
-	} catch (err) {
-		console.error("Error executing sql: " + err)
-	}
+  try {
+    const config = {
+      user: instance.MasterUsername,
+      password: process.env.RDS_PASSWORD,
+      server: instance.Endpoint.Address,
+      database: "tempdb",
+      options: {
+        enableArithAbort: true,
+      },
+    };
+    console.log("connecting to " + instance.Endpoint.Address);
+    await sql.connect(config);
+    const result = await sql.query(process.env.SQL_SCRIPT);
+    console.dir(result);
+  } catch (err) {
+	console.error("Error executing sql: " + err);
+	context.fail("Error executing sql: " + err);
+  }
 };
